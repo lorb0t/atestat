ad29c7753f5f5aa323e75d6bf47cfa3a47e1bca9
progenor <nero6126@gmail.com>
2022-04-15 13:37:29 +0300

    video_sound

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,13 +1,19 @@
 import { useRef, useState } from "react";
 import "./Video.scss";
 
-import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import {
+  BsFillPlayFill,
+  BsFillPauseFill,
+  BsFillVolumeMuteFill,
+  BsFillVolumeUpFill,
+} from "react-icons/bs";
 
 import video from "../../assets/Videos/video-intro.mp4";
 
 export const Video = () => {
   const vidRef = useRef();
   const [playVideo, setPlayVideo] = useState(false);
+  const [playSound, setPlaySound] = useState(true);
 
   const handleVideo = () => {
     setPlayVideo((prevPlayVideo) => !prevPlayVideo);
@@ -19,27 +25,54 @@ export const Video = () => {
     }
   };
 
+  const handleSound = () => {
+    setPlaySound((prevPlaySound) => !prevPlaySound);
+
+    if (playSound) {
+      vidRef.current.muted = true;
+    } else {
+      vidRef.current.muted = false;
+    }
+  };
+
   return (
     <div className="video_main">
       <video
         src={video}
         type="video/mp4"
         loop
-        controls={true}
-        muted
+        controls={false}
         ref={vidRef}
         onClick={handleVideo}
       />
       {playVideo ? (
-        ""
+        <div className="mute-btn-overlay-active">
+          <div className="mute-btn" onClick={handleSound}>
+            {playSound ? (
+              <BsFillVolumeUpFill color="white" fontSize={35} />
+            ) : (
+              <BsFillVolumeMuteFill color="white" fontSize={35} />
+            )}
+          </div>
+        </div>
       ) : (
+        // ""
         <div className="video-overlay">
           <div className="video-overlay-circle" onClick={handleVideo}>
             {playVideo ? (
-              <BsFillPauseFill color="white" fontSize={30} />
+              <BsFillPauseFill color="white" fontSize={40} />
             ) : (
-              <BsFillPlayFill color="white" fontSize={30} />
+              <BsFillPlayFill color="white" fontSize={40} />
             )}
+          </div>
+          <div className="mute-btn-overlay">
+            <div className="mute-btn" onClick={handleSound}>
+              {playSound ? (
+                <BsFillVolumeUpFill color="white" fontSize={35} />
+              ) : (
+                <BsFillVolumeMuteFill color="white" fontSize={35} />
+              )}
+            </div>
           </div>
         </div>
       )}
