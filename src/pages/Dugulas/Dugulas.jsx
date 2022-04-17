9c5c84617b8aa101846efddc9a4e8a965f28788a
progenor <nero6126@gmail.com>
2022-04-17 13:02:36 +0300

    dugulas

--- src/pages/Dugulas/Dugulas.jsx
+++ src/pages/Dugulas/Dugulas.jsx
@@ -2,8 +2,33 @@ import "./Dugulas.scss";
 
 export const Dugulas = () => {
   return (
-    <div className="more-main">
-      <div>szia</div>
+    <div className="dugulas-main">
+      <div className="dugulas-text">
+        <p>
+          Ha eldugult a mosdó, zuhanyzó, padlóösszefolyó, kádlefolyó, WC
+          lefolyó, vagy a főlefolyó - jó helyen jár - mi igazán kihúzzuk önt a
+          ... baj- ból! Belső lefolyóvezetékek duguláselhárítására használt
+          eszközeink a megszokott (kígyó) spirál továbbfejlesztett gépesített
+          változata, amelyek egymásba csatlakoztathatók és végein cserélhető
+          fejek végzik a leghatékonyabb munkát. A keletkezett dugó
+          megszűntetésére megfelelő tisztítófejet használva teljesen
+          eltávolítható a cső teljes belső átmérőjéből a lerakódás,
+          szennyeződés. Gyakran előfordul hogy a WC illatosító beleesik és
+          belesszorul a lefolyóvezetékbe. Régebben ennek az eltávolítását
+          bontással lehetett megoldani, ez könnyen megoldható korszerű
+          felszerelésünkkel. A WC dugulását okozhatja a felhalmozódott toalett
+          papír, vagy a nem lefolyóba illő dolgok mint a tisztasági betét, ruha
+          darabok, nedves törlőkendő, papír törlőkendő, macskaalom, vagy
+          bármilyen tárgy ami nem oldódik a vízben. Az udvari lefolyók, a
+          főlefolyók gyakran lerakódnak zsírral, homokkal, falevelek
+          torlaszolják a működését, vagy rosszul megtervezett, rosszul
+          megépített csatornarendszer is lehet az oka, a lerakódott csőszakasz
+          tisztítását nagynyomású vízsúgárral végezzük el. Udvari vagy utcai
+          lefolyórendszer duguláselhárítását, karbantartását vállaljuk DN600
+          átmérőig, kamionra felszerelt kombi nagynyomású (szippantó - mosó)
+          járművekkel.{" "}
+        </p>
+      </div>
     </div>
   );
 };
